2110cecbe9c0b8a5ec760dc42120e85ebc10bcf2
lioryahav@gmaill.com <lioryahav@gmail.com>
2021-12-22 11:53:13 +0200

    added notebooks about encoding, classes, modules

---
+++ notebooks/research.py
@@ -0,0 +1,23 @@
+'''
+class supplier():
+    level = 1
+
+class distributor():
+    level = 1
+
+class shop_warehouse(supplier, distributor):
+    level = 2
+
+class shop_dispatch(shop_warehouse):
+    level = 3
+
+class shelf(shop_dispatch, distributor):
+    level = 3
+
+print(shelf.level)
+'''
+
+print('hello world')
+x = 5
+x = x + x
+print(x)

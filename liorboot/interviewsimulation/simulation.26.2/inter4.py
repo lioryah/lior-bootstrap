cc91fb24e8e1183c22606aab1cf41fbcc2b9fa8f
lioryahav@gmaill.com <lioryahav@gmail.com>
2022-03-07 18:26:05 +0200

    search for autocompletion done. todo add timer

--- liorboot/interviewsimulation/simulation.26.2/inter4.py
+++ liorboot/interviewsimulation/simulation.26.2/inter4.py
@@ -4,7 +4,7 @@ import inter3
 # read tokens to db in dictionary form
 def append_to_db4 (db_dict_tokens, token) -> dict:
     if token == '':
-        db_dict_tokens[None] = None
+        db_dict_tokens['_end'] = '_end'
         return db_dict_tokens
     
     first_char = token[0]
@@ -13,9 +13,9 @@ def append_to_db4 (db_dict_tokens, token) -> dict:
         db_dict_tokens[first_char] = {}
         if len(token) == 1:
             rest = ''
-        else:
-            rest = token[1:]
-        append_to_db4(db_dict_tokens[first_char], rest)
+    rest = token[1:]
+    append_to_db4(db_dict_tokens[first_char], rest)
+    
     return db_dict_tokens
 
 def load_tokens4(fpath):
@@ -25,30 +25,75 @@ def load_tokens4(fpath):
         append_to_db4(db_dict_tokens, token)
     return db_dict_tokens
 
-def nested_dict_search4(my_db : dict, prefix : str):
-    if prefix == None:
-        return ''
-    else:
-        if prefix[0] in my_db[prefix]:
-            return  prefix[0] + nested_dict_search4(my_db[prefix], prefix[1:])
-            
-
-def get_completions4(my_db : dict, prefix : str) -> list:
+# returns a list of tokens from my_db
+def flatten_dict(my_db : dict) -> list:
+    from pandas.io.json._normalize import nested_to_record    
     res = []
-    ctr = 0
-    while ctr < 20:
-        word = nested_dict_search4(my_db, prefix)
-        res.append(word)
-        ctr += 1
 
+    flat = nested_to_record(my_db, sep='')
+    for key in flat:
+        if key.endswith('_end'):
+            
+            tmp = key[0:-4]
+            res.append(tmp)
+    return res
+
+def measure_time(func):
+    import timeit
+    def inner (*args, **kwargs):    
+        print("time elpased for get_completions4 " + str(timeit.timeit(func(*args, **kwargs))))
+
+    return inner
+
+@measure_time
+def get_completions4(my_db : dict, prefix : str) -> list:
+    flat_dict = flatten_dict(my_db)
+    
+    res = []
+    i = 0
+    ctr = 0
+    while ctr < 20 and i < len(flat_dict):
+        if flat_dict[i].startswith(prefix):
+            res.append(flat_dict[i])
+            ctr += 1
+        i += 1
     return res
 
 
+# %%
+# test 1
+my_db = {}
 my_db = load_tokens4("tokens.txt")
+
+print('my_db =')
 print(my_db)
-# %%
 
+#%%
+
+prefix = 'ba'
+print('tokens that have prefix \'' + prefix + '\':')
+print(get_completions4(my_db, prefix))
+
+#%%
+
+prefix = 'ra'
+print('tokens that have prefix \'' + prefix + '\':')
+print(get_completions4(my_db, prefix))
+
+#%%
+
+prefix = 'mar'
+print('tokens that have prefix \'' + prefix + '\':')
+print(get_completions4(my_db, prefix))
+
+# %% different tokens
+my_db = {}
+my_db = load_tokens4("tokens2.txt")
+print(my_db)
 
 # %%
-print(get_completions4(my_db, "baron"))
+prefix = 'bal'
+print('tokens that have prefix \'' + prefix + '\':')
+print(get_completions4(my_db, prefix))
+
 # %%

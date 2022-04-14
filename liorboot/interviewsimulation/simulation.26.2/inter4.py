0fb6747b9fff52adcdea591dfc1200f48b0064ac
lioryahav@gmaill.com <lioryahav@gmail.com>
2022-04-14 17:28:03 +0300

    added rest lecture

--- liorboot/interviewsimulation/simulation.26.2/inter4.py
+++ liorboot/interviewsimulation/simulation.26.2/inter4.py
@@ -1,4 +1,8 @@
 #%%
+from importlib.resources import path
+from inspect import stack
+from tkinter.messagebox import NO
+from typing import Iterable, List
 import inter3
 
 # read tokens to db in dictionary form
@@ -45,9 +49,11 @@ def measure_time(func):
 
     return inner
 
-
+'''
 def iter_suffixes(my_db):
     yield ''
+
+
 
 # agrregator 
 def collect_suffixes(my_db, limit):
@@ -57,7 +63,7 @@ def collect_suffixes(my_db, limit):
             break
         res.append(suffix)
     return res
-
+'''
 
 # @measure_time
 # TODO. seperate function to 2 fuctions: get subtree after prefix. 
@@ -77,6 +83,84 @@ def get_completions4(my_db: dict, prefix: str, limit: int = 20) -> list:
         i += 1
     return res
 
+################################
+def find_prefix(mdb:dict, prefix: str) -> dict:
+    if prefix == '':
+        return mdb
+    else:
+        first = prefix[0]
+        if first in mdb:
+            return find_prefix(mdb[first], prefix[1:])
+
+
+def get_single_suffix(sub_dict:dict, res:str='', pathlist:List=[]):
+    for k in sub_dict:
+        
+        if k != '_end':
+            res += k 
+            pathlist.append(k)
+            next(get_single_suffix(sub_dict[k], res, pathlist))     
+        elif sub_dict[k] == {}:
+            sub_dict.pop(k)
+        elif k == '_end':
+            sub_dict.pop(k)
+            yield res    
+
+'''
+def get_single_suffix_2(sub_dict:dict, res:str='', pathlist:List=[]):        
+    stack = []
+    curr_node = sub_dict
+
+    while True:
+        if curr_node is not '_end':
+            curr_node = curr_node[0]
+
+        else:
+            break
+'''
+
+def iterate_suffixes2(mdb:dict) -> Iterable[str]:
+    
+    curr = mdb
+    res = ''
+    while True:
+        for k in curr.keys():
+            sub_node = curr[k]
+            if k == '_end':
+                res.join(k)
+                yield res
+            else:
+                curr = curr[k]
+                continue
+    
+
+
+
+def iterate_suffixes(mdb:dict):
+    root_db = {'root' : mdb}
+    yield get_single_suffix(root_db['root'])
+    
+  
+
+def get_completions(my_db: dict, prefix: str, limit: int = 20) -> list:
+    completions = []
+    if my_db == {}:
+        return completions
+    suffix_dict = find_prefix(my_db, prefix)
+     
+    i=0
+    while i<limit:
+        single_suffix = iterate_suffixes2(suffix_dict)
+        completions.append(next(single_suffix))
+        i+=1
+    return completions
+
+# recursive method that find the prefix in the trie-dict and return a dict that contain the suffixes for a specific prefix 
+
+def retrive_suffixes_by_prefix(mdb, prefix, limit) -> List[str]:
+    pass
+
+
 
 # %%
 # test 1
@@ -85,6 +169,16 @@ my_db = load_tokens4("tokens.txt")
 
 print('my_db =')
 print(my_db)
+
+
+# get_completions_fedor(my_db, 'ba')
+completions_list = get_completions(my_db, 'ba')
+print(completions_list)
+
+# %%
+db_suffix = find_prefix(my_db, 'ba')
+print(db_suffix)
+
 
 #%%
 
@@ -114,4 +208,7 @@ prefix = 'bal'
 print('tokens that have prefix \'' + prefix + '\':')
 print(get_completions4(my_db, prefix))
 
+# %%
+strtest = 'a'
+strtest[2:]
 # %%

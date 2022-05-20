6a24d5d90dfcd2ce0e3e1996dad51a910d8eff13
lioryahav@gmaill.com <lioryahav@gmail.com>
2022-05-20 22:32:43 +0300

    added uploading to mongodb data from file

--- liorboot/mongodb/mongodb_tutorial_demo.py
+++ liorboot/mongodb/mongodb_tutorial_demo.py
@@ -1,11 +1,13 @@
-
+# from pymongo import MongoClient
 import pymongo
 import dns
 import datetime
+import fire
+from pathlib import Path
+
 
 
 def pymongo_demo(connection_conf:str):
-
     if connection_conf == "cluster0":
         # Replace the uri string with your MongoDB deployment's connection string.
         conn_str = 'mongodb+srv://lior:lior123@cluster0.xeqbp.mongodb.net/test?retryWrites=true&w=majority'
@@ -13,6 +15,7 @@ def pymongo_demo(connection_conf:str):
         # set a 5-second connection timeout
         client = pymongo.MongoClient(conn_str)
         db = client.test
+    
     if connection_conf == "datalake0":
         client = pymongo.MongoClient("mongodb://lior:lior123@datalake0-xeqbp.a.query.mongodb.net/myFirstDatabase?ssl=true&authSource=admin")
         db = client.test
@@ -23,29 +26,52 @@ def pymongo_demo(connection_conf:str):
     except Exception:
         print("Unable to connect to the server.")
 
-    
+    # add_person_doc(client)
+    upload_data_from_file("2466_tokens.txt" , client)
+
+def add_person_doc(client:pymongo.MongoClient):
     db = client.gettingStarted
 
     people = db.people
 
     personDocument = {
-        "name": {"first": "Alan", "last": "Turing"},
-        "birth": datetime.datetime(1912, 6, 23),
-        "death": datetime.datetime(1954, 6, 7),
-        "contribs": ["Turing machine", "Turing test", "Turingery"],
+        "name": {"first": "Ice", "last": "Cube"},
         "views": 1250000
     }
 
-    personDocument2 = {
-        "name": {"first": "Lior", "last": "Yahav"},
-        "birth": datetime.datetime(1987, 10, 13),
-        "views": 123456
-    }
-
-    # people.insert_one(personDocument2)
+    people.insert_one(personDocument)
 
     print(people.find_one({"name.last": "Yahav"}))
 
+def upload_data_from_file(file_name:str, client:pymongo.MongoClient):
+    db = client.tokens
+
+    tokens = db.tokens2466
+    
+    data_file = open(file_name, "r")
+    data = data_file.readlines()
+    
+    for data_entry in data:
+        print(data_entry)
+        tokens.insert_one({"token":data_entry.strip()})
+
+    print("Done with uploading from file {}", file_name)
+
+def upload_data_from_file_generator(file_name:str, client:pymongo.MongoClient):
+    db = client.tokens
+
+    tokens = db.tokens2466
+    
+    tokens.insert_one(next(get_data_from_file_generator(file_name)))
+    
+
+def get_data_from_file_generator(file_name:str):
+    data_file = open(file_name, "r")
+    data = data_file.readlines()
+    data_entry = data
+
+    while data_entry!=None:
+        yield data_entry
 
 
 def _main():

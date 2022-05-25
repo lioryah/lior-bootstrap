7f0e96da74851e822a7cff44864fcc416b24f2b0
lioryahav@gmaill.com <lioryahav@gmail.com>
2022-05-25 14:47:08 +0300

    delete not fully working. fix access to db and d collection

--- liorboot/mongodb/mongodb_tutorial_demo.py
+++ liorboot/mongodb/mongodb_tutorial_demo.py
@@ -1,4 +1,5 @@
 # from pymongo import MongoClient
+from matplotlib.collections import Collection
 import pymongo
 import dns
 import datetime
@@ -6,28 +7,26 @@ import fire
 from pathlib import Path
 
 
-
-def pymongo_demo(connection_conf:str):
-    if connection_conf == "cluster0":
+# return pymongo client_db 
+def pymongo_demo(connection_conf:str, db_name:str):
+    if connection_conf=="cluster0" and db_name=="tokens":
         # Replace the uri string with your MongoDB deployment's connection string.
-        conn_str = 'mongodb+srv://lior:lior123@cluster0.xeqbp.mongodb.net/test?retryWrites=true&w=majority'
+        conn_str = 'mongodb+srv://lior:lior123@cluster0.xeqbp.mongodb.net/tokens?retryWrites=true&w=majority'
 
         # set a 5-second connection timeout
         client = pymongo.MongoClient(conn_str)
-        db = client.test
     
     if connection_conf == "datalake0":
         client = pymongo.MongoClient("mongodb://lior:lior123@datalake0-xeqbp.a.query.mongodb.net/myFirstDatabase?ssl=true&authSource=admin")
-        db = client.test
 
     try:
-        print("server information")
+        print("server information: \n\n")
         print(client.server_info())
     except Exception:
         print("Unable to connect to the server.")
 
-    # add_person_doc(client)
-    upload_data_from_file("2466_tokens.txt" , client)
+    return client
+
 
 def add_person_doc(client:pymongo.MongoClient):
     db = client.gettingStarted
@@ -43,6 +42,7 @@ def add_person_doc(client:pymongo.MongoClient):
 
     print(people.find_one({"name.last": "Yahav"}))
 
+
 def upload_data_from_file(file_name:str, client:pymongo.MongoClient):
     db = client.tokens
 
@@ -55,7 +55,9 @@ def upload_data_from_file(file_name:str, client:pymongo.MongoClient):
         print(data_entry)
         tokens.insert_one({"token":data_entry.strip()})
 
-    print("Done with uploading from file {}", file_name)
+    data_file.close()
+    print(f"Done with uploading from file {file_name}")
+
 
 def upload_data_from_file_generator(file_name:str, client:pymongo.MongoClient):
     db = client.tokens
@@ -74,8 +76,29 @@ def get_data_from_file_generator(file_name:str):
         yield data_entry
 
 
+def delete_many(client:pymongo.MongoClient, collection_name:str):
+    # db = client.tokens
+    # db = client.tokens
+
+    # tokens = db.tokens2466
+   
+    # db.{collection}.delete_many({})
+    if collection_name=="2466tokens":
+        client.tokens2466.delete_many({})
+
 def _main():
-    pymongo_demo("cluster0")
+    client = pymongo_demo("cluster0", "tokens")
+   
+    # add_person_doc(client)
+    
+    # file_name = "2466_tokens.txt"
+    # upload_data_from_file(file_name , client)
+    
+    file_name = "10_tokens.txt"
+    upload_data_from_file(file_name , client)
+
+    delete_many(client, "tokens2466")
+
 
 if __name__ == '__main__':
     _main()
